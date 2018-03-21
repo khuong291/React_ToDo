bf18d2658ce16bb6c1aba78d11523d9218e62f3b
khuong291 <dkhuong291@gmail.com>
2018-03-21 18:00:30 +0700

    Add actions

---
+++ src/components/App.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import AddTodo from '../containers/AddTodo';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <AddTodo />
+      </div>
+    );
+  }
+}
+
+export default App;
